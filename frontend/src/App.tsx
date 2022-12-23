959f64d6f390e27687a23dfe627e30951f961903
kc0506 <kchong0506@gmail.com>
2022-12-23 15:01:04 +0800

    Basic 3D scene test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,52 @@
-import { useState } from 'react'
+import { Sky } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useState, useRef } from 'react'
 import './App.css'
+import styled from 'styled-components';
+import { AxesHelper, PointLightHelper } from 'three';
+import { OrbitControls, PerspectiveCamera, useHelper } from '@react-three/drei/core';
 
 interface IProps {
-	name: string,
-	func?: <T>(x: T) => T,
+
 };
+
+const Box = () => {
+
+	const ref = useRef<THREE.Mesh>(null!);
+	useFrame(() => ref.current!.rotation.y += 0.01)
+
+	return (
+		<mesh ref={ref} position={[0, 1, -3]} >
+			<boxGeometry args={[5, 2, 2]} />
+			<meshStandardMaterial args={[{ color: 'red' }]} />
+		</mesh>
+	)
+}
+
+const Lights = () => {
+	const ref = useRef<THREE.PointLight>(null!);
+	useHelper(ref, PointLightHelper, 1, 'blue');
+
+	return < pointLight ref={ref} color={'white'} position={[5, 5, 0]} />
+}
 
 function App(props: IProps) {
 
-	const [count, setCount] = useState<number>(0);
-
 	return (
-		<div className="App">
-			<p>Hello world!</p>
-		</div>
+		<>
+			<Canvas >
+				<axesHelper args={[10]} />
+				<gridHelper />
+				<ambientLight color={'grey'} />
+				<OrbitControls />
+				<PerspectiveCamera
+					makeDefault
+					position={[5, 10, 10]}
+				/>
+				<Lights />
+				<Box />
+			</Canvas>
+		</>
 	)
 }
 
