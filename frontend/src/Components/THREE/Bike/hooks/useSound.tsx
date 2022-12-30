5f0a1efc013a5f85294947db811d80396e63132a
kc0506 <kchong0506@gmail.com>
2022-12-31 04:31:39 +0800

    add bell sound

--- frontend/src/Components/THREE/Bike/hooks/useSound.tsx
+++ frontend/src/Components/THREE/Bike/hooks/useSound.tsx
@@ -7,24 +7,53 @@ interface IProps {
 };
 
 
-const sound = new Howl({
+const sound1 = new Howl({
     src: './sounds/bicycle.mp3',
     autoplay: true,
     loop: true,
 });
 
+const sound2 = new Howl({
+    src: './sounds/bicycle2.mp3',
+    autoplay: true,
+    loop: true,
+});
+
+const bell = new Howl({
+    src: './sounds/bike-bell.mp3',
+    volume: 0.25,
+});
+
+const lingLing = (e: KeyboardEvent) => {
+    if (e.key === '1')
+        bell.play();
+};
+
 const minSpeed = 15;
 const maxSpeed = 30;
+
+const totalVolume = 1;  // should be a context state in the future
 
 export default function useSound({ speed }: IProps) {
 
     useEffect(() => {
-        sound.play();
-        return () => { sound.stop(); }
+        sound1.play();
+        sound2.play();
+
+        window.addEventListener('keypress', lingLing);
+
+        return () => {
+            sound1.stop();
+            sound2.stop();
+            window.removeEventListener('keypress', lingLing);
+        }
     }, []);
 
     useFrame(() => {
-        sound.rate(Math.sqrt(Math.sqrt(speed / minSpeed)));
-        sound.volume(Math.pow(speed / maxSpeed, 2));
+        sound1.rate(Math.sqrt(Math.sqrt(speed / minSpeed)));
+        sound1.volume(Math.pow(speed / maxSpeed, 2) * totalVolume);
+
+        sound2.volume(Math.sqrt(speed / maxSpeed * 0.2) * totalVolume);
     })
+
 }
