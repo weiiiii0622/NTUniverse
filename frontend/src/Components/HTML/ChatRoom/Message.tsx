801a9a0ac6ebedae537eac08d3374c0ca71f33c5
wwchin <kevinwang920422@gmail.com>
2023-01-05 00:07:25 +0800

    chatroom with email

--- frontend/src/Components/HTML/ChatRoom/Message.tsx
+++ frontend/src/Components/HTML/ChatRoom/Message.tsx
@@ -10,30 +10,50 @@ const StyledMessage = styled.div<{ isMe: boolean }>`
     align-items: center;
     justify-content: flex-start;
     flex-direction: ${({ isMe }) => (isMe ? 'row-reverse' : 'row')};
-    margin: 8px 10px;
-    gap: 10px;
+    margin: 0px 2px;
+    gap: 5px;
 
     & div:last-child {
-        margin: 0 5px;
-        padding: 2px 5px;
-        border-radius: 5px;
-        border: #eee solid 2px;
-        background: #eee;
-        color: gray;
-        margin: auto 0;
+      margin: 0 0px;
+      padding: 2px 5px;
+      border-radius: 5px;
+      border: #eee solid 2px;
+      background: #eee;
+      color: gray;
+      margin: auto 0;
     }
 `;
 
+const StyledName = styled.p<{ isMe: boolean }>`
+  margin: 0;
+  padding-left: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: ${({ isMe }) => (isMe ? 'row-reverse' : 'row')};
+
+  & p {
+    margin: 0;
+    font-size: 12px;
+    color: #74726D;
+  }
+`;
+
 const Message = (props: IMessage) => {
-    const { sender, content } = props;
-    const { me } = useMyContext();
-    const myEmail = me['email'];
-    const isMe = sender === myEmail;
-    return (
-        <StyledMessage isMe={isMe}>
-            {!isMe && <SmileTwoTone />}<div>{content}</div>
-        </StyledMessage>
-    );
+  const { sender, content } = props;
+  const { me } = useMyContext();
+  const myEmail = me['email'];
+  const isMe = sender === myEmail;
+  return (
+    <>
+      <StyledName isMe={isMe}>
+        <p>{sender}</p>
+      </StyledName>
+      <StyledMessage isMe={isMe}>
+        {!isMe && <SmileTwoTone style={{ fontSize: '20px' }} />} <div>{content}</div>
+      </StyledMessage>
+    </>
+  );
 }
 
 export default Message;
