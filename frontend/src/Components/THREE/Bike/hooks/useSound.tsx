c86a30b1207f9fae397373f1c2e961db3c6fd340
weiiiii0622 <weiiiii.0622@gmail.com>
2023-01-05 08:19:15 +0800

    update bug

--- frontend/src/Components/THREE/Bike/hooks/useSound.tsx
+++ frontend/src/Components/THREE/Bike/hooks/useSound.tsx
@@ -48,7 +48,7 @@ export default function useSound({ speed }: IProps) {
     const { volumeValue } = useBikeContext();
 
     useEffect(() => {
-        if (bikeEnabled)
+        //if (bikeEnabled)
             //console.log('hi')
     }, [bikeEnabled]);
 
